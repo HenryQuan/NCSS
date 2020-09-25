b60fe220c711b6467f2e2c2420ef53305ffb8fed
Yiheng (Mac) <development.henryquan@gmail.com>
2020-09-25 23:33:48 +1000

    update to version 1.0.1
    lets see if it works

--- sample/Injection/ViewController.swift
+++ sample/Injection/ViewController.swift
@@ -12,11 +12,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        showVerionAlert()
+        valueLabel.text = "\(defaultValue())"
+    }
+    
+    func showVerionAlert() {
+        let pref = UserDefaults.standard
+        // Get app version
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+        let displayed = pref.bool(forKey: appVersion)
+        // Show this only once
+        if !displayed {
+            let alert = UIAlertController(title: "Version 1.0.1", message: "A new upgrade with some minor changes :)", preferredStyle: .alert)
+            alert.addAction(.init(title: "Close", style: .default))
+            self.present(alert, animated: true)
+            pref.setValue(true, forKey: appVersion)
+        }
     }
     
     @IBAction func onTapMeButtonPressed(_ sender: Any) {
         let value = Int(valueLabel.text!)!
         valueLabel.text = "\(value + 1)"
+    }
+    
+    func defaultValue() -> Int {
+        var value = 0
+        for i in 0...100 {
+            value += i
+        }
+        return value
     }
 }
 
--- sample/binary/README.md
+++ sample/binary/README.md
@@ -1,3 +1,10 @@
 # binary
 - `appstore`, binary downloaded from app store
 - `dump`, binary dumpped from memory
+
+### 1.0.1
+- Added an alert view
+- Set initial value to 5050
+
+### 1.0
+- The initial version with basic functionalities

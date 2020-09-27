38f45b3f28f59358b14c2717f5c688ff7e094187
Yiheng (Mac) <development.henryquan@gmail.com>
2020-09-28 08:59:04 +1000

    use karen's mthod
    it writes to file

--- zhao/Tweak.x
+++ zhao/Tweak.x
@@ -5,8 +5,7 @@ BOOL score;
 
 static void reloadPrefs() {
 	NSLog(@"Notification received");
-	[[NSUserDefaults standardUserDefaults] synchronize];
-	NSDictionary *pref = [[NSUserDefaults standardUserDefaults] persistentDomainForName:@PREF_DOMAIN];
+	NSDictionary *pref = [[NSMutableDictionary alloc] initWithContentsOfFile:@PLIST_PATH] ?: [@{} mutableCopy];
 	NSLog(@"Pref: %@", [pref description]);
 	score = [[pref objectForKey:@"score"] ?: @(NO) boolValue];
 	NSLog(@"Score: %d", score);
--- zhao/zhaopref/HQRootListController.m
+++ zhao/zhaopref/HQRootListController.m
@@ -10,4 +10,24 @@
 	return _specifiers;
 }
 
+// Karen's method, https://iphonedevwiki.net/index.php/PreferenceBundles#Loading_Preferences_into_sandboxed.2Funsandboxed_processes_in_iOS_8
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+	[settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:path]];
+	return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+	[settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:path]];
+	[settings setObject:value forKey:specifier.properties[@"key"]];
+	[settings writeToFile:path atomically:YES];
+	CFStringRef notificationName = (CFStringRef)specifier.properties[@"PostNotification"];
+	if (notificationName) {
+		CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL, NULL, YES);
+	}
+}
+
 @end

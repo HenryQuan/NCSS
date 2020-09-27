d17fb546a37c655b6af2098f56fa3a852112fcaa
Yiheng (Mac) <development.henryquan@gmail.com>
2020-09-27 10:31:35 +1000

    working again, the size was the cause

--- sample/Injection/ViewController.swift
+++ sample/Injection/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         showVerionAlert()
         valueLabel.text = "\(defaultValue())"
+        onTapMeButtonPressed(self)
     }
     
     func showVerionAlert() {

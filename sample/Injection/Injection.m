df6d29eddbb1b976559c0a481686a3c24a8960af
Yiheng (Mac) <development.henryquan@gmail.com>
2020-10-03 22:56:24 +1000

    started working on searching multiple values

--- sample/Injection/Injection.m
+++ sample/Injection/Injection.m
@@ -11,8 +11,8 @@
 
 +(void)inject
 {
-    vm_address_t address = vm_searchData("2A0500B1E8779F1AEA2F00F9", [AppTool getBinarySize]);
-    vm_writeData("2A9D0FB1", address);
+//    vm_address_t address = vm_searchData("2A0500B1E8779F1AEA2F00F9", [AppTool getBinarySize]);
+//    vm_writeData("2A9D0FB1", address);
 }
 
 +(void)loadPref
@@ -20,6 +20,19 @@
     NSDictionary *pref = [NSDictionary dictionaryWithContentsOfFile:@PLIST_PATH];
     BOOL score = [[pref valueForKey:@"enabled"] boolValue];
     NSLog(@"Score is %d", score);
+}
+
++(void)test
+{
+    Module a[] = {
+        { 200, 100, "EACD", 123 },
+        { 200, 100, "ADCE", 123 },
+        { 200, 100, "1234", 123 },
+        { 200, 100, "5321", 123 },
+        { 200, 100, "1234", 123 },
+    };
+    int size = sizeof(a) / sizeof(a[0]);
+    vm_searchData((Module *)&a, size, 100);
 }
 
 @end

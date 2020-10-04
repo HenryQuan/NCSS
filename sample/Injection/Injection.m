bb7d978eb0582267309340c7203b137be9781c6f
Yiheng (Mac) <development.henryquan@gmail.com>
2020-10-04 12:25:07 +1100

    values are not updated
    is there a way to do this?

--- sample/Injection/Injection.m
+++ sample/Injection/Injection.m
@@ -24,15 +24,17 @@
 
 +(void)test
 {
-    Module a[] = {
-        { 200, 100, "EACD", 123 },
-        { 200, 100, "ADCE", 123 },
-        { 200, 100, "1234", 123 },
-        { 200, 100, "5321", 123 },
-        { 200, 100, "1234", 123 },
+    const int size = 5;
+    Module a[size] = {
+        { 0, "", "EACD", 0 },
+        { 0, "", "2A0500B1E8779F1AEA2F00F9", "EACD", 0 },
+        { 0, "", "EACD", "1234", 0 },
+        { 0, "", "EACD", "5321", 0 },
+        { 0, "", "EACD", "1234", 0 },
     };
-    int size = sizeof(a) / sizeof(a[0]);
-    vm_searchData((Module *)&a, size, 100);
+    vm_searchData(&a, size, [AppTool getBinarySize]);
+    Module one = a[1];
+    printf("Pause\n");
 }
 
 @end

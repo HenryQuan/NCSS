a898bfe947b28a72017ac770cc732a8ccbbffd8d
Yiheng (DTK) <development.henryquan@gmail.com>
2020-10-09 17:54:49 +1100

    update build and header path

--- zhao/Tweak.h
+++ zhao/Tweak.h
@@ -1,6 +1,6 @@
 
-#include "helper/app_tool.h"
-#include "helper/vm_tool.h"
+#include "../vm_tools/app_tool.h"
+#include "../vm_tools/vm_tool.h"
 
 #define PLIST_PATH "/var/mobile/Library/Preferences/org.github.henryquan.zhao.plist"
 #define PREF_CHANGED "org.github.henryquan.zhao/PrefChanged"

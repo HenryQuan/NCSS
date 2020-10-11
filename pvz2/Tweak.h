98fdcff057c383e9144f136945cb0a4441cbc53a
Yiheng (Mac) <development.henryquan@gmail.com>
2020-10-11 18:51:57 +1100

    test pvz2 on ipod

--- pvz2/Tweak.h
+++ pvz2/Tweak.h
@@ -1,6 +1,5 @@
 
-#include "../zhao/helper/app_tool.h"
-#include "../zhao/helper/vm_tool.h"
+#include "../vm_tools/Tweak.h"
 
 #define PLIST_PATH "/var/mobile/Library/Preferences/org.github.henryquan.pvz2.plist"
 #define PREF_CHANGED "org.github.henryquan.pvz2/PrefChanged"
